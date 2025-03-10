c02fb64bb0d29d53eceb544469b56baf771df083
restonic4 <marcosruizlerma@gmail.com>
2025-03-10 14:54:24 +0100

    idk

---
+++ src/main/java/com/restonic4/versatilesanity/mixin/PlayerRendererMixin.java
@@ -0,0 +1,22 @@
+package com.restonic4.versatilesanity.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerRenderer.class)
+public class PlayerRendererMixin {
+    @Inject(
+            method = "render(Lnet/minecraft/client/player/AbstractClientPlayer;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    public void render(AbstractClientPlayer abstractClientPlayer, float f, float g, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, CallbackInfo ci) {
+        ci.cancel();
+    }
+}
